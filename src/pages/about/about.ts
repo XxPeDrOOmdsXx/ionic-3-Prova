52e24f70759d3de59428e787be933dd8dc669f30
XxPeDrOOmdsXx <pedromds90@gmail.com>
2018-01-26 20:38:21 -0200

    first commit

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public navparams: NavParams,
+    public alervctrl: AlertController) {
 
+  }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TestPage');
+  }
+  
+  showAlert() {
+    let alert = this.alervctrl.create({
+      title: 'Mensagem',
+      subTitle: 'Cadastrado com Sucesso',
+      buttons: ['OK']
+    });
+    alert.present();
+    
   }
 
 }
